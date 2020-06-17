f3b07cfb8773449359aa237c0e08c81100ca1807
naomi <iam.naomi92@gmail.com>
2020-06-17 23:16:18 +0900

    c5

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
 function turnRed(e) {
-  const red =document.querySelector('');
 
-   key.addEventListener('keydown', evt => {
+  const red =document.querySelector(`.key[data-key="65"]`);
+  console.log(red);
 
-   });
+   // key.addEventListener('keydown', evt => {
+   //
+   // });
+
   // for (let key of red){
   //   key.classList.add('playing');
   //   console.log();
@@ -21,3 +24,8 @@ function turnRed(e) {
 //
 //   });
 // }
+
+// class名の取得をクエリでする場合は、querySelector('.class名')と表現します。タグ名はquerySelector('タグ名')です
+  // ヒント1: HTMLにあるデータ属性の取得表現は、querySelector('[data属性名="data属性内の値"]')です
+  // ヒント2: class名.keyのdata属性data-key="キー番号"の取得は、上記の組み合わせですね。テンプレートリテラルを使うとひとまとめにできます
+  // querySelector(`.class名[data属性名="data属性内の値"]`) と、書けそうです
