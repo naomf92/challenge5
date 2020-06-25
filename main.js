465561f72be2f59ab558d8dc4ec77f8f5cf4cd39
naomi <iam.naomi92@gmail.com>
2020-06-25 23:52:08 +0900

    challenge5

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 
 function turnRed(e) {
-
   const element =document.querySelector(`.key[data-key="${e.keyCode}"`);
-  console.log(element);
-
-  window.addEventListener('keydown',turnRed);{
-      element.classList.add('playing');
+  for (let key of element) {
+    key.classList.add('playing');
   }
-
  }
- turnRed();
+ window.addEventListener('keydown',turnRed);
 
-// const action =document.querySelectorAll('[data-action="addStyle"]');
-// console.log(action[0].dataset);
-//
 
-const sound =document.querySelector('.sound');
-console.log(sound);
+// function play(e) {
+//   const sound = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//   console.log(sound);
+// }
+// window.addEventListener('keydown',play);
 
