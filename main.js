d108f34c8e7af9a3fbdc006e2400051d6637b8f3
naomi <iam.naomi92@gmail.com>
2020-06-30 21:25:45 +0900

    challenge5

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 
-function turnRed(e) {
-  const element =document.querySelector(`.key[data-key="${e.keyCode}"`);
+function effect(e) {
+  const element = document.querySelector(`.key[data-key="${e.keyCode}"`);
   element.classList.add('playing');
+
+  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(sound);
+  sound.play();
  }
- window.addEventListener('keydown',turnRed);
+ window.addEventListener('keydown',effect);
+
+ 
+// function turnRed(e) {
+//   const element = document.querySelector(`.key[data-key="${e.keyCode}"`);
+//   element.classList.add('playing');
+//  }
+//  window.addEventListener('keydown',turnRed);
 
 
 // function play(e) {
-//   const sound = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//   const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 //   console.log(sound);
+//   sound.play();
 // }
 // window.addEventListener('keydown',play);
 
