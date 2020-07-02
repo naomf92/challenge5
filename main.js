7f7f98e2c2bd0ceaee1bf4e6a9c73277c41f8d4a
naomi <iam.naomi92@gmail.com>
2020-07-02 18:52:11 +0900

    challenge5

--- main.js
+++ main.js
@@ -10,17 +10,8 @@ function effect(e) {
  window.addEventListener('keydown',effect);
 
  
-// function turnRed(e) {
-//   const element = document.querySelector(`.key[data-key="${e.keyCode}"`);
-//   element.classList.add('playing');
-//  }
-//  window.addEventListener('keydown',turnRed);
-
-
-// function play(e) {
-//   const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//   console.log(sound);
-//   sound.play();
-// }
-// window.addEventListener('keydown',play);
-
+function effectOff(e) {
+  const element = document.querySelector(`.key[data-key="${e.keyCode}"`);
+  element.classList.remove('playing');
+}
+window.addEventListener('keyup',effectOff);
