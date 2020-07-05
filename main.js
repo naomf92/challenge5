8be70176067eb45276cd866f264a4032ab03453f
naomi <iam.naomi92@gmail.com>
2020-07-05 12:00:00 +0900

    challenge5

--- main.js
+++ main.js
@@ -13,5 +13,9 @@ function effect(e) {
 function effectOff(e) {
   const element = document.querySelector(`.key[data-key="${e.keyCode}"`);
   element.classList.remove('playing');
+
+  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  sound.pause();
+  sound.currentTime = 0;
 }
 window.addEventListener('keyup',effectOff);
